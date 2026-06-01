8cfe9f4aa04664fcfe2d9cde17f7e0f5b0d68051
Nikhil <nikhilsharma666123@gmail.com>
2026-06-01 16:10:59 +0530

    Update q20

---
+++ Day-5/q20.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int num, i, largest = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    for (i = 2; i <= num; i++) {
+        while (num % i == 0) {
+            largest = i;
+            num = num / i;
+        }
+    }
+
+    printf("Largest Prime Factor = %d", largest);
+
+    return 0;
+}
