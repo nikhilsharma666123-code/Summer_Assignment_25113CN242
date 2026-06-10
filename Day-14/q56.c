0aeb6c581fd98a36ab5a4463f8eacae574bd8c6a
Nikhil <nikhilsharma666123@gmail.com>
2026-06-10 23:58:34 +0530

    Day-14 uploaded

---
+++ Day-14/q56.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int a[10], n, i, j;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Duplicate elements: ");
+
+    for(i = 0; i < n; i++) {
+        for(j = i + 1; j < n; j++) {
+            if(a[i] == a[j]) {
+                printf("%d ", a[i]);
+            }
+        }
+    }
+
+    return 0;
+}
