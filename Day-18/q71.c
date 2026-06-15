195375fd4b09eb2f249b283da0f8c5481d7b7256
Nikhil <nikhilsharma666123@gmail.com>
2026-06-15 18:16:44 +0530

    Day-18 uploaded

---
+++ Day-18/q71.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, key, low, high, mid;
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    int a[n];
+
+    printf("Enter sorted elements: ");
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    low = 0;
+    high = n - 1;
+
+    while(low <= high) {
+        mid = (low + high) / 2;
+
+        if(a[mid] == key) {
+            printf("Element found at position %d", mid + 1);
+            return 0;
+        }
+        else if(a[mid] < key)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    printf("Element not found");
+
+    return 0;
+}
